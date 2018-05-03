195c90ac39c01c139a8401768843ba5e02796a5f
Ryan Holstein <Ryan.c.Holstein@biola.edu>
2018-05-03 16:40:44 -0700

    added all projects

--- src/components/Content/Projects/Project/ProjectInfo.js
+++ src/components/Content/Projects/Project/ProjectInfo.js
@@ -8,7 +8,21 @@ let projectInfo = {
     riddlefamilyFoundation : {
         title: 'riddle',
         desc: 'this is the desc for riddle'
+    },
+    cianmoore : {
+        title: 'Cian Moore',
+        desc: 'This was a website for CM'
+    },
+    twothumbsup : {
+        title: 'Two Thumbs Up',
+        desc: 'this was two thumbs project'
+    },
+    createsafebeta : {
+        title : 'CreateSafe Beta',
+        desc: 'This project was the createsafe beta'
     }
+    
+
 }
 
 export default projectInfo;
